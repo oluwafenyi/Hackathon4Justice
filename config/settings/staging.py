5ad003b3bcb7079f4ad0cf3cc1a28ff4d6849ece
Enyioma Osondu <o.enyioma@gmail.com>
2020-02-07 18:13:24 +0100

    staging prep

---
+++ config/settings/staging.py
@@ -0,0 +1,13 @@
+from .base import *
+
+import dj_database_url
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    '.herokuapp.com'
+]
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
