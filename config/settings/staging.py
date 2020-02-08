fc1c9dbef9ee65947b3c1166756fc31e4f5e38a2
Enyioma Osondu <o.enyioma@gmail.com>
2020-02-08 14:51:45 +0100

    whitenoise settings

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -5,6 +5,11 @@ import dj_database_url
 
 DEBUG = True
 
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    *MIDDLEWARE
+]
+
 ALLOWED_HOSTS = [
     '.herokuapp.com'
 ]
@@ -13,6 +18,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
