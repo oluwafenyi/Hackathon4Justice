2c236c6165553d819ccb578ef36bede09e3d2ce2
Enyioma Osondu <o.enyioma@gmail.com>
2020-02-08 14:46:46 +0100

    cors settings modified

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -9,9 +9,8 @@ ALLOWED_HOSTS = [
     '.herokuapp.com'
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    '*',
-]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
